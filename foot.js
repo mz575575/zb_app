669647c508668a26a9318e959002ea53c441e985
maomao <821041971@qq.com>
2020-12-31 10:57:57 +0800

    直播jQuery模板

---
+++ foot.js
@@ -0,0 +1,6 @@
+document.writeln("<div class=\'container\'>");
+document.writeln("        <p><script id=\'jsgovicon\' src=\'static/js/govicon.js\' type=\'text/javascript\' charset=\'utf-8\'></script> © 2020 安徽觅柏网络科技有限公司保留所有权利&nbsp;&nbsp;");
+document.writeln("");
+document.writeln("        &nbsp;&nbsp;公司地址：安徽省合肥市高新区科学大道55号综合楼二楼2-207号 &nbsp;&nbsp;联系电话：0551-83653588&nbsp;&nbsp;   QQ：3124406849</p>");
+document.writeln("        <div class=\'line-style\'></div>");
+document.writeln("    </div>");
